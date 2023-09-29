ecb363cdb9e74d552aa855c80607da4327221bcd
Reversive <matiaspavan@gmail.com>
2023-09-29 16:40:13 -0300

    feat: new input system + cinemachine package for player camera movement

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    #region PRIVATE_PROPERTIES
+    private CharacterController _controller;
+    private Vector3 _playerVelocity;
+    private bool _groundedPlayer;
+    private InputManager _inputManager;
+    [SerializeField] private float _playerSpeed = 2.0f;
+    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _gravityValue = -9.81f;
+    private Transform _cameraTransform;
+    #endregion
+
+    #region UNITY_METHODS
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _inputManager = InputManager.Instance;
+        _cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        _groundedPlayer = _controller.isGrounded;
+        if (_groundedPlayer && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = _inputManager.GetMovementInput();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = _cameraTransform.forward * move.z + _cameraTransform.right * move.x;
+        _controller.Move(move * Time.deltaTime * _playerSpeed);
+
+ 
+        if (_inputManager.IsJumpPressed() && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+        }
+
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        _controller.Move(_playerVelocity * Time.deltaTime);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    #region PRIVATE_PROPERTIES
+    private PlayerControls _playerControls;
+    private static InputManager _instance;
+    #endregion
+
+    #region PUBLIC_PROPERTIES
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    #endregion
+
+    #region UNITY_METHODS
+    private void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+            _instance = this;
+        }
+        _playerControls = new PlayerControls();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+    #endregion
+
+    #region PUBLIC_METHODS
+    public Vector2 GetMovementInput()
+    {
+        return _playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+    public Vector2 GetMouseDelta()
+    {
+        return _playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool IsJumpPressed()
+    {
+        return _playerControls.Player.Jump.triggered;
+    }
+    #endregion
+}
