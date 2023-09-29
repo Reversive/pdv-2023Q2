486a8b444c57819ced763a55c6ef7cc38ec96038
Reversive <matiaspavan@gmail.com>
2023-09-29 20:32:10 -0300

    feat: flashlight sound implementation

--- Assets/Scripts/Entities/Flashlight.cs
+++ Assets/Scripts/Entities/Flashlight.cs
@@ -1,17 +1,48 @@
 
 using UnityEngine;
 
-public class Flashlight : MonoBehaviour
+public class Flashlight : MonoBehaviour, IListener
 {
     #region PRIVATE_PROPERTIES
     [SerializeField] private Light _light;
+    [SerializeField] private AudioClip _audioClip;
+    private AudioSource _audioSource;
     private InputManager _inputManager;
+    #endregion
+
+    #region PUBLIC_PROPERTIES
+    public AudioClip AudioClip => _audioClip;
+    public AudioSource AudioSource => _audioSource;
+    #endregion
+
+    #region SOUND_METHODS
+    public void InitAudioSource()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _audioClip;
+    }
+
+    public void Play()
+    {
+        _audioSource.Play();
+    }
+
+    public void PlayOneShot()
+    {
+        _audioSource.PlayOneShot(_audioClip);
+    }
+
+    public void Stop()
+    {
+       _audioSource.Stop();
+    }
     #endregion
 
     #region UNITY_METHODS
     private void Start()
     {
         _inputManager = InputManager.Instance;
+        InitAudioSource();
     }
 
     private void Update()
@@ -19,7 +50,9 @@ public class Flashlight : MonoBehaviour
         if (_inputManager.IsFlashlightPressed())
         {
             _light.enabled = !_light.enabled;
+            PlayOneShot();
         }
+
     }
     #endregion
 }
