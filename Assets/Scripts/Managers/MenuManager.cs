6927a9cbd006b1d33046b5dbabf5bdebd678f175
santigarciam <sangarcia@itba.edu.ar>
2023-09-30 16:50:11 -0300

    feat: main menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-   public void ButtonPlayPressed() => Debug.Log( "TEST");
+   public void ButtonPlayPressed() => Invoke("LoadEndgameScene", 3f);
+   private void LoadEndgameScene() => SceneManager.LoadScene(0);
 }
